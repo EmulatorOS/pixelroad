30d8d5515e3e27532347a08126f1e4ddb060968c
Kevin <kevinsalvador2008@outlook.com>
2022-07-25 17:59:16 -0500

    first

---
+++ shared/lib.js
@@ -0,0 +1,13 @@
+!(function () {
+  if (window.UnityLoader) {
+    if (window.UnityLoader.compatibilityCheck) {
+      window.UnityLoader.compatibilityCheck = function (
+        element,
+        callback,
+        errCallback
+      ) {
+        callback();
+      };
+    }
+  }
+})();
